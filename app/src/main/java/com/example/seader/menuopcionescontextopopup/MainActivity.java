4e006bfd10369463511fc0822e9d6b5bf42d96a6
dafma <mar_tp@hotmail.es>
2017-02-15 20:22:09 -0600

    creado menu parte superior derecha

--- app/src/main/java/com/example/seader/menuopcionescontextopopup/MainActivity.java
+++ app/src/main/java/com/example/seader/menuopcionescontextopopup/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.seader.menuopcionescontextopopup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //este metodo nos creara un menu de opciones
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones, menu);
+        return true;
     }
 }
