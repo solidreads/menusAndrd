677569ad55defc6bf84d174546f53aae7e540a40
dafma <mar_tp@hotmail.es>
2017-02-15 20:50:40 -0600

    creando acciones a los botones del menu

--- app/src/main/java/com/example/seader/menuopcionescontextopopup/MainActivity.java
+++ app/src/main/java/com/example/seader/menuopcionescontextopopup/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.seader.menuopcionescontextopopup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_opciones, menu);
         return true;
+    }
+
+    // con este metodo se controla lo que se hara con las opciones
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.mAbout:
+                Intent intent = new Intent(this, ActivityAbout.class);
+                startActivity(intent);
+                break;
+
+            case R.id.mSettings:
+                Intent i = new Intent(this, ActivitySettings.class);
+                startActivity(i);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
