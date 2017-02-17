528e164bb76fa65efed0a1bccb1f477b5181f675
dafma <mar_tp@hotmail.es>
2017-02-17 13:32:44 -0600

    menu popup

--- app/src/main/java/com/example/seader/menuopcionescontextopopup/MainActivity.java
+++ app/src/main/java/com/example/seader/menuopcionescontextopopup/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.seader.menuopcionescontextopopup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,5 +54,44 @@ public class MainActivity extends AppCompatActivity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         getMenuInflater().inflate(R.menu.menu_contexto, menu);
+    }
+
+    //para el item de contexto seleccionado
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.mEdit:
+                Toast.makeText(this, getResources().getString(R.string.menu_edit), Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mDelete:
+                Toast.makeText(this, getResources().getString(R.string.menu_delete), Toast.LENGTH_SHORT).show();
+
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+    //menu pop up
+    public void levantarmenuPopUp(View v){
+        ImageView imagen = (ImageView) findViewById(R.id.imgImagen);
+        PopupMenu popupMenu = new PopupMenu(this, imagen);
+        popupMenu.getMenuInflater().inflate(R.menu.menu_popup, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem item){
+                switch (item.getItemId()){
+                    case R.id.mView:
+                        Toast.makeText(getBaseContext(), getResources().getString(R.string.menu_view), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.mViewDetail:
+                        Toast.makeText(getBaseContext(), getResources().getString(R.string.menu_view_detail), Toast.LENGTH_LONG).show();
+
+                        break;
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
     }
 }
